a5cfccc2d914ca899b9d61cf129271d74e6ef99c
Gabi Fabiyi <gabrielbambamfabiyi@outlook.com>
2025-09-08 13:30:23 +0100

    added multihead class for self attention and cross attention

--- mumble-app/src/bot/MultiHead.py
+++ mumble-app/src/bot/MultiHead.py
@@ -1,9 +1,26 @@
 import torch
 from torch import nn
+from Head import Head
 
 class MultiHead(nn.Module):
 
-    def __init__(self, d_model, n_head, head_size, vocab_size, dropout, device, mask=True):
+    def __init__(self, d_model, n_head, head_size, vocab_size, dropout, block_size, device, mask=True):
 
         super().__init__()
+
+        self.heads = nn.Sequential(
+            *[Head(block_size, d_model, head_size, dropout, device, mask=True) for _ in range(n_head)]
+        )
+
+        self.proj = nn.Linear(
+            in_features=head_size,
+            out_features=d_model,
+            device=device
+        )
+        self.dropout = dropout
+
+    def forward(self, x, context=None):
+        out = torch.cat([h(x, context) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
 
